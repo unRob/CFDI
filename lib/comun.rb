b511e2ee821839fdd38165bc8b73a4a6f8eecc09
rob <rob@partidosurrealista.com>
2014-12-03 19:36:53 -0600

    no se porqué mierdas monkyepatcheé float...

--- lib/comun.rb
+++ lib/comun.rb
@@ -1,26 +1,26 @@
-# Mkay, quiero que mis floats tengan dos decimales a huevo
-class Float
-  
-  # Lo que dice la clase
-  # 
-  # @return [String] El string de este float con dos decimales
-  def to_s
-    sprintf('%.2f', self)
-  end
-  
-end
+# # Mkay, quiero que mis floats tengan dos decimales a huevo
+# class Float
+
+#   # Lo que dice la clase
+#   #
+#   # @return [String] El string de este float con dos decimales
+#   def to_s
+#     sprintf('%.2f', self)
+#   end
+
+# end
 # Y quiero que los strings no tengan espacios extraños
 class String
 
   # Limpia whitespace de extremos y espacios repetidos
-  # 
+  #
   # @return [String] Una copia del string sin espacios extraños
   def squish
     dup.squish!
   end
 
   # Lo mismo que squish, pero destructivo
-  # 
+  #
   # @return [String] El string original sin espacios extraños
   def squish!
     strip!
@@ -35,11 +35,11 @@ module CFDI
 
   # Un elemento del comprobante con métodos mágicos y especiales
   class ElementoComprobante
-    
+
 
     # Crear este elemento y settear lo que le pasemos como hash en un tipo de dato adecuado
     # @param  data [Hash] Los datos para este elemento
-    # 
+    #
     # @return [CFDI::ElementoComprobante] El elemento creado
     def initialize data={}
       #puts self.class
@@ -52,7 +52,7 @@ module CFDI
 
 
     # Los elementos para generar la cadena original de este comprobante
-    # 
+    #
     # @return [Array] idem
     def self.data
       @cadenaOriginal
@@ -60,21 +60,21 @@ module CFDI
 
 
     # Un array con los datos de la cadena original para este elemento
-    # 
+    #
     # @return [Array] idem
     def cadena_original
       params = []
       data = {}
       data = self.class.data
      # puts self.class.cadenaOriginal
-      
+
       data.each {|key| params.push instance_variable_get('@'+key.to_s) }
       return params
     end
 
 
     # Los datos xmleables de este elemento
-    # 
+    #
     # @return [Hash] idem
     def to_h
       h = {}
@@ -83,10 +83,10 @@ module CFDI
         value = value.to_h if value.is_a? ElementoComprobante
         h[v] = value
       end
-      
+
       h
     end
-    
+
   end
 
 end
